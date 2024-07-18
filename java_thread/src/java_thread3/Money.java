50a8aef8136951d8f656c3617bf4418c4e93225c
kyerim1 <nilzero25@gmail.com>
2024-07-18 17:33:07 +0900

    7월 18일

---
+++ java_thread/src/java_thread3/Money.java
@@ -0,0 +1,27 @@
+package java_thread3;
+
+public class Money {
+	private int myMoney = 10000;
+	public int getMyMoney() {
+		return myMoney;
+	}
+	// widhdraw메서드를 임계영역으로 설정
+	public synchronized boolean widhdraw(int money) {
+		if( myMoney >= money) {
+	// 현재 보유 금액 보다 출금금액이 작거나 같다면 출금
+			try {
+			Thread.sleep(1000);
+			}catch(Exception e) {}
+			
+			//출금
+			myMoney -= money;
+			return true;
+		}
+		return false; // 출금 실패(돈부족)
+	}
+}
+
+
+
+
+
